3de16007a7434e2c1c70bccfa484e12f5553900c
Emir the Great <emirergun@live.com>
2026-07-26 22:49:52 +0300

    a

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        pathname: "/**", // Allows all sub-paths under cdn.sanity.io
       },
     ],
   },
